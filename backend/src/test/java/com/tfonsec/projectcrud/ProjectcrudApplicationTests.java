6883f98422f63795bac15e9aff8d602056e508d5
Thiago Fonseca <thifonseca85@gmail.com>
2021-07-22 19:43:40 -0300

    backend

---
+++ backend/src/test/java/com/tfonsec/projectcrud/ProjectcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfonsec.projectcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
