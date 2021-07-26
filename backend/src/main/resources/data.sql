f57e154be06ef6b091c2c4a8087b813b5f084cd5
Thiago <thifonseca85@gmail.com>
2021-07-26 15:10:28 -0300

    Seeddatabase

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Obi Wan Kenobi', '61569038015', 4000.0, TIMESTAMP WITH TIME ZONE '1936-04-05T00:00:00Z', 2)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Sauron da Silva', '06416860058', 5000.0, TIMESTAMP WITH TIME ZONE '1980-02-09T00:00:00Z', 1)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ganondorf Ferreira Junior', '34837716032', 6000.0, TIMESTAMP WITH TIME ZONE '1996-09-12T00:00:00Z', 1)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Leia Skywalker', '92689612020', 7000.0, TIMESTAMP WITH TIME ZONE '1940-01-01T00:00:00Z', 3)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Zelda Lullaby Ocarina', '24253625069', 8000.0, TIMESTAMP WITH TIME ZONE '1989-04-08T00:00:00Z', 2)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lara Croft', '57310507088', 9000.0, TIMESTAMP WITH TIME ZONE '2000-07-03T00:00:00Z', 2)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Dovakiin Silvestre', '91717670024', 10000.0, TIMESTAMP WITH TIME ZONE '1936-10-11T00:00:00Z', 1)
