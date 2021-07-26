c1180bd4119bd4e7f4fc8272158e779dbf4a4bcf
Thiago <thifonseca85@gmail.com>
2021-07-26 18:18:19 -0300

    get and put client

--- backend/src/main/java/com/tfonsec/projectcrud/resources/ClientResource.java
+++ backend/src/main/java/com/tfonsec/projectcrud/resources/ClientResource.java
@@ -1,5 +1,6 @@
 package com.tfonsec.projectcrud.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,8 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tfonsec.projectcrud.dto.ClientDTO;
 import com.tfonsec.projectcrud.entities.Client;
@@ -32,6 +37,22 @@ public class ClientResource {
 		ClientDTO dto  = service.findById(id);
 		return ResponseEntity.ok().body(dto);
   }
+	
+	@PostMapping
+	public  ResponseEntity <ClientDTO> insert(@RequestBody ClientDTO dto) {
+		dto = service.insert(dto);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+	               .buildAndExpand(dto.getId()).toUri();
+	    	return ResponseEntity.created(uri).body(dto);
+	    }
+	
+	 @PutMapping(value = "/{id}")
+	    public ResponseEntity<ClientDTO> update (@PathVariable Long id, @RequestBody ClientDTO dto) {
+	    	dto = service.update(id,dto);
+	    	return ResponseEntity.ok().body(dto);
+	    
+    }   
 }
 
 
