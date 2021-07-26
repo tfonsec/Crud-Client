fbb60f8c8cbccfc97202399d650411dea6c6aa62
Thiago <thifonseca85@gmail.com>
2021-07-26 15:48:01 -0300

    DTO

--- backend/src/main/java/com/tfonsec/projectcrud/resources/ClientResource.java
+++ backend/src/main/java/com/tfonsec/projectcrud/resources/ClientResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tfonsec.projectcrud.dto.ClientDTO;
 import com.tfonsec.projectcrud.entities.Client;
 import com.tfonsec.projectcrud.servicies.ClientService;
 
@@ -20,8 +21,8 @@ public class ClientResource {
 	private ClientService service;
 	
 	@GetMapping
-	public  ResponseEntity <List<Client>> findAll() {
-		List<Client> list  = service.findAll();
+	public  ResponseEntity <List<ClientDTO>> findAll() {
+		List<ClientDTO> list  = service.findAll();
 		return ResponseEntity.ok().body(list);
   }
 }
--- backend/src/main/java/com/tfonsec/projectcrud/servicies/ClientService.java
+++ backend/src/main/java/com/tfonsec/projectcrud/servicies/ClientService.java
@@ -1,10 +1,15 @@
 package com.tfonsec.projectcrud.servicies;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.tfonsec.projectcrud.dto.ClientDTO;
 import com.tfonsec.projectcrud.entities.Client;
 import com.tfonsec.projectcrud.repositories.ClientRepository;
 
@@ -14,8 +19,9 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	   public List <Client> findAll() {
-	   return repository.findAll();
-	   
+	@Transactional(readOnly = true)
+	public List <ClientDTO> findAll() {
+	   List<Client> list = repository.findAll();
+	   return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	   }
 }
