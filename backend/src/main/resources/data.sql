9e78f835c541262be242f1018b854f479fbad84d
Thiago <thifonseca85@gmail.com>
2021-07-26 19:19:36 -0300

    Complete

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -5,3 +5,7 @@ INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Leia Sk
 INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Zelda Lullaby Ocarina', '24253625069', 8000.0, TIMESTAMP WITH TIME ZONE '1989-04-08T00:00:00Z', 2)
 INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lara Croft', '57310507088', 9000.0, TIMESTAMP WITH TIME ZONE '2000-07-03T00:00:00Z', 2)
 INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Dovakiin Silvestre', '91717670024', 10000.0, TIMESTAMP WITH TIME ZONE '1936-10-11T00:00:00Z', 1)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Gandalf the Grey', '2268971201', 10000.0, TIMESTAMP WITH TIME ZONE '1900-05-01T00:00:00Z', 6)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Tom Marvollo Ridlle', '14855922200', 8000.0, TIMESTAMP WITH TIME ZONE '1985-11-08T00:00:00Z', 2)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Tyrion Lannister', '14589632101', 9000.0, TIMESTAMP WITH TIME ZONE '2001-08-07T00:00:00Z', 2)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Hermione Granger ', '11737620024', 10000.0, TIMESTAMP WITH TIME ZONE '1995-02-11T00:00:00Z', 1)
